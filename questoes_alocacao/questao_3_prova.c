fcab73859d42feb397999fd54e397899cbeb8a36
João Luis <joaoluisd2005@gmail.com>
2024-12-30 15:42:44 -0300

    uptade

--- questoes_alocacao/questao_3_prova.c
+++ questoes_alocacao/questao_3_prova.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-float **cria_matriz(int linhas, int colunas);
+float** cria_matriz(int linhas, int colunas);
 float** soma_matriz(float** A, int n, int m, float** B); 
-void libera_matriz(float **A,int linhas);
+void libera_matriz(float** A, int linhas);
 int main()
 {
     float **a = cria_matriz(2,2);
     float **b = cria_matriz(2,2);
     float **c = soma_matriz(a,2,2,b);
-    printf("Matriz resultante:\n");
+    printf("MATRIZ RESULTANTE: ");
     for(int i=0;i<2;i++)
     {
         for(int j=0;j<2;j++)
         {
-            printf("%.2f\n",c[i][j]);
+            printf("%f\n",c[i][j]);
         }
     }
     libera_matriz(a,2);
     return 0;
 }
-float **cria_matriz(int linhas, int colunas)
+float** cria_matriz(int linhas, int colunas)
 {
-    float **matriz=(float**)malloc(linhas*sizeof(float*));
+    float **matriz = (float**)malloc(linhas*sizeof(float*));
     for(int i=0;i<linhas;i++)
     {
         matriz[i]=(float*)malloc(colunas*sizeof(float));
@@ -34,18 +34,18 @@ float **cria_matriz(int linhas, int colunas)
 }
 float** soma_matriz(float** A, int n, int m, float** B)
 {
-    float **soma_matriz=(float**)malloc(2*sizeof(float*));
+    float **matriz_resultante=(float**)malloc(n*sizeof(float*));
     for(int i=0;i<n;i++)
     {
-        soma_matriz[i]=(float*)malloc(2*sizeof(float));
+        matriz_resultante[i]=(float*)malloc(m*sizeof(float));
         for(int j=0;j<m;j++)
         {
-            soma_matriz[i][j] = A[i][j] + B[i][j];
+            matriz_resultante[i][j]=A[i][j]+B[i][j];
         }
     }
-    return soma_matriz;
+    return matriz_resultante;
 }
-void libera_matriz(float **A,int linhas)
+void libera_matriz(float** A, int linhas)
 {
     for(int i=0;i<linhas;i++)
     {
