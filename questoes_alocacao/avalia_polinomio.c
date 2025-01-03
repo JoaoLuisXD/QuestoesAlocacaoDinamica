5344f91ec6a2049b6224a1122d2f4a8d0c4bf8d9
João Luis <joaoluisd2005@gmail.com>
2025-01-03 17:56:20 -0300

    update

--- questoes_alocacao/avalia_polinomio.c
+++ questoes_alocacao/avalia_polinomio.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 double avalia (double *poli,int grau,double x);
 int main()
 {
-    
+    int grau;
+    printf("DIGITE O GRAU DO POLINOMIO: ");
+    scanf("%d%*c",&grau);
+    double *polinomio = (double*)malloc(grau*sizeof(double));
+    for(int i=0;i<=grau;i++)
+    {
+        printf("DIGITE O %d TERMO(grau %d): ",i+1,i);
+        scanf("%lf%*c",&polinomio[i]);
+    }
+    double x;
+    printf("DIGITE O VALOR DE X: ");
+    scanf("%lf%*c",&x);
+    printf("O RESULTADO DO POLINOMIO DIGITADO E: %.2lf",avalia(polinomio,grau,x));
     return 0;
 }
 double avalia (double *poli,int grau,double x)
 {
-
+    double resultado=0;
+    for(int i=0;i<grau;i++)
+    {
+        resultado += poli[i] * pow(x,i);
+    }
+    return resultado;
 }
