341ea712d191a06a3383535c7931e76005dce0c2
João Luis <joaoluisd2005@gmail.com>
2025-01-07 22:51:26 -0300

    update

--- questoes_alocacao/deriva_polinomio.c
+++ questoes_alocacao/deriva_polinomio.c
@@ -8,7 +8,7 @@ int main()
     printf("DIGITE O GRAU DO POLINOMIO: ");
     scanf("%d%*c",&grau);
     double *polinomio = (double*)malloc(grau*sizeof(double));
-    double *derivada = (double*)malloc(sizeof(double));
+    double *derivada = (double*)malloc(grau*sizeof(double));
     for(int i=0;i<=grau;i++)
     {
         printf("DIGITE O %d NUMERO (grau %d): ",i+1,i);
@@ -17,14 +17,14 @@ int main()
     deriva(polinomio,grau,derivada);
     for (int i = 0; i < grau; i++) 
     {
-        printf("COEFICIENTE DO TERMO DE GRAU %d: %.2lf\n", i, derivada[i]);
+        printf("COEFICIENTE DO TERMO DE GRAU %d: %.2lf\n", i+1, derivada[i]);
     }
 
     return 0;
 }
 void deriva(double *poli,int grau,double *dervd)
 {
-    for(int i=1;i<=grau;i++)
+    for(int i=1;i<=grau;i++) //não pega o primeiro termo, pq sempre vai ser zero
     {
         dervd[i-1]=poli[i]*i;
     }
